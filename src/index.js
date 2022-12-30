7b748fd60cfbbef7ea05295c5db6d4be3b8d531d
iamchacha021 <iamchacha021@gmail.com>
2022-12-30 23:21:38 +0300

    completed

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
 const init = () => {
+
+    const inputForm = document.querySelector('form')
+    inputForm.addEventListener('submit', (event)=>{
+        event.preventDefault()
+        const input = inputForm.querySelector('input[type="text"]')
+        console.log(input.value)
+
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then((response)=>{
+            return response.json()
+        })
+        .then((data)=>{
+            const title = document.querySelector('#movieDetails h4');
+            const summary = document.querySelector('#movieDetails p');
+            title.innerText = data.title
+            summary.innerText = data.summary
+        })
+    })
   
 }
 
